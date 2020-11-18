e523e4c4755bec51ecaf20e12ff95cdbdf7e69ca
jordansmont <54561201+jordansmont@users.noreply.github.com>
2020-11-18 15:50:35 -0800

    Add files via upload

--- assignment_08/question_02/CISCresults.sql
+++ assignment_08/question_02/CISCresults.sql
@@ -8,18 +8,18 @@ Show NID, name, employer, job title, extracurricular group and position, course 
 .output CISCresults.out
 
 SELECT
-  Employees.NID,
-  Employees.First_Name,
-  Employees.Last_Name,
-  Employees.Department,
-  Employees.Job_Title,
-  Extracurriculars.Program,
-  Extracurricularsta.Position,
-  StudentRegister.Course1,
-  StudentRegister.Course2
+  AllData.nid,
+  AllData.First_Name,
+  AllData.Last_Name,
+  AllData.Department,
+  AllData.Job_Title,
+  AllData.Program,
+  AllData.Position,
+  AllData.Course1,
+  AllData.Course2
 FROM
   (SELECT
-	Employees.NID,
+	Employees.nid,
   	Employees.First_Name,
   	Employees.Last_Name,
   	Employees.Department,
@@ -30,28 +30,28 @@ FROM
   	StudentRegister.Course2
    FROM 
 	Employees
-   LEFT JOIN Extracurriculars ON Employees.NID = Extracurriculars.NID
-   LEFT JOIN StudentRegister ON Employees.NID = StudentRegister.NID
+   LEFT JOIN Extracurriculars ON Employees.nid = Extracurriculars.NID
+   LEFT JOIN StudentRegister ON Employees.nid = StudentRegister.NID
    UNION
    SELECT
-	Extracurriculars.NID,
-	Extracurriculars.First_Name,
-	Extracurriculars.Last_Name
+	Employees.NID,
+	Extracurriculars.FirstName,
+	Extracurriculars.LastName,
 	Employees.Department,
-	Employees.Job_Title,
+	Employees.Job_Title,	
 	Extracurriculars.Program,
 	Extracurriculars.Position,
 	StudentRegister.Course1,
 	StudentRegister.Course2
    FROM
 	Extracurriculars
-   LEFT JOIN Employees ON Extracurriculars.NID = Employees.NID
+   LEFT JOIN Employees ON Extracurriculars.NID = Employees.nid
    LEFT JOIN StudentRegister ON Extracurriculars.NID = StudentRegister.NID
    UNION
    SELECT
-	StudentRegister.NID,
-	StudentRegister.First_Name,
-	StudentRegister.Last_Name
+	Employees.NID,
+	StudentRegister.FirstName,
+	StudentRegister.LastName,
 	Employees.Department,
 	Employees.Job_Title,
 	Extracurriculars.Program,
@@ -62,7 +62,7 @@ FROM
 	StudentRegister
    LEFT JOIN Employees ON StudentRegister.NID = Employees.NID
    LEFT JOIN Extracurriculars ON StudentRegister.NID = Extracurriculars.NID 
-   )
+   );
 
 .output stdout
 

50456cd5a469020edd28acdea0b754967761fa93
jordansmont <54561201+jordansmont@users.noreply.github.com>
2020-11-18 11:21:15 -0500

    Update HighestBid01.sql

--- assignment_08/question_01/HighestBid01.sql
+++ assignment_08/question_01/HighestBid01.sql
@@ -3,6 +3,10 @@ Question 1A
 Calculate the highest bid for each auction and store with the associated Auction ID
 */
 
+.mode column
+.headers on
+.output HighestBid01.out
+
 SELECT
 AUCTIONS.AUCTIONID,
 MAX(BIDS.BID) AS HIGHESTBID
@@ -12,3 +16,5 @@ INNER JOIN BIDS AS BIDS
 ON AUCTIONS.AUCTIONID = BIDS.AUCTIONID
 GROUP BY
 AUCTIONS.AUCTIONID;
+
+.output stdout
